2889c5eab5b323780d4dc5b101b3d2dbb5c09f37
addilewis21 <addilewis21@gmail.com>
2021-10-25 15:02:31 -0600

    attempted to fix some thigns

--- characters/sw-characters.js
+++ characters/sw-characters.js
@@ -1,11 +1,41 @@
-import { people } from '../data/people.js'
-
-const mainContent = document.querySelector('#main')
-
-people.forEach(element => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
-    charFigure.appendChild(charImg)
-    mainContent.appendChild(charFigure)
+/*jshint esversion: 6 */
+import {
+	people
 }
+from '../data/people.js';
+
+const mainBody = document.querySelector('body');
+const header = document.createElement('header');
+const maleButton = document.createElement('button');
+maleButton.textContent = 'Male Characters';
+const femaleButton = document.createElement('button');
+femaleButton.textContent = 'Female Characters';
+header.appendChild(maleButton);
+header.appendChild(femaleButton);
+mainBody.appendChild(header);
+
+
+const mainContent = document.querySelector('#main');
+
+
+const maleCharacters = people.filter(person => person.gender === 'male');
+const femaleCharacters = people.filter(person => person.gender === 'female');
+
+console.log(maleCharacters);
+console.log(femaleCharacters);
+
+people.forEach((element, index) => {
+	const charFigure = document.createElement('figure');
+	const charImg = document.createElement('img');
+	charImg.src = `https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg`;
+
+	const charCaption = document.createElement('figcaption');
+	charCaption.textContent = `${element.name} at index ${index}`;
+
+
+
+	charFigure.appendChild(charImg);
+	charFigure.appendChild(charCaption);
+	charFigure.appendChild(mainContent);
+
+});
