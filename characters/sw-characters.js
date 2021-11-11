9503e95a3c4bcca07cd76a778e4645f7ad6e4061
addilewis21 <addilewis21@gmail.com>
2021-11-11 11:42:15 -0700

    added another button

--- characters/sw-characters.js
+++ characters/sw-characters.js
@@ -1,54 +1,56 @@
-/*jshint esversion: 6 */
-import { people } from '../data/people.js';
-import { getLastNumber, removeChildren } from '../utils/index.js';
+import { people } from '../data/people.js'
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
+const mainContent = document.querySelector('#main')
 
-const mainContent = document.querySelector("#main");
+const maleCharacters = people.filter((person) => person.gender === 'male')
+const femaleCharacters = people.filter((person) => person.gender === 'female')
+const otherCharacters = people.filter((person) => {
+  if (
+    person.gender !== 'male' &&
+    person.gender !== 'female' 
+  ) {
+    return person
+  }
+})
 
-const maleCharacters = people.filter((person) => person.gender === "male");
-const femaleCharacters = people.filter((person) => person.gender === "female");
-const otherCharacters = people.filter(person => {
-	if (
-		person.gender !== 'male'&& 
-		person.gender !== 'female'
-	   ){
-		return person;
-	}
-});
+const header = document.createElement('header')
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+//populateDOM(people)
 
-const header = document.createElement("header");
-const maleButton = document.createElement("button");
-maleButton.textContent = "Male Characters";
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
 
-populateDOM(otherCharacters);
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
 
-maleButton.addEventListener("click", () => populateDOM(maleCharacters));
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 
-const femaleButton = document.createElement("button");
-femaleButton.textContent = "Female Characters";
+const othersButton = document.createElement('button')
+othersButton.textContent = 'Other Characters'
 
-femaleButton.addEventListener('click', () => populateDOM(femaleCharacters));
+othersButton.addEventListener('click', () => populateDOM(otherCharacters))
 
-header.appendChild(maleButton);
-header.appendChild(femaleButton);
+header.appendChild(maleButton)
+header.appendChild(femaleButton)
+header.appendChild(othersButton)
 
-document.body.insertBefore(header, mainContent);
+document.body.insertBefore(header, mainContent)
 
 function populateDOM(characters) {
-	// clear the page first, then populate
+  // clear the page first, then populate
   removeChildren(mainContent)
   characters.forEach((element) => {
-    const charFigure = document.createElement("figure");
-    const charImg = document.createElement("img");
-    const charNum = getLastNumber(element.url);
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+    const charNum = getLastNumber(element.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
 
-    const charCaption = document.createElement("figcaption");
-    charCaption.textContent = element.name;
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = element.name
 
-    charFigure.appendChild(charImg);
-    charFigure.appendChild(charCaption);
-    mainContent.appendChild(charFigure);
-  });
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
+  })
 }
-
